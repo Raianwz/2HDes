281070bda85e789d536b41f2e61b49e20dc6097d
raianwz <raian03@live.com>
2020-03-10 22:07:36 -0300

    Finalizado Calculo do IMC e INSS

--- ProjetoJava/src/PacoteJava/Inss.java
+++ ProjetoJava/src/PacoteJava/Inss.java
@@ -7,14 +7,32 @@ public class Inss {
     public static void main(String[] args) {
         String aux = " ";
         double salario = 0, aliquota = 0, valordesconto = 0, valorliquido = 0;
-
+        
         aux = JOptionPane.showInputDialog(null, "Digite seu salário", "Salário", -1);
-        aux =  aux.replace(',', '.');
+        aux = aux.replace(',', '.');
         salario = Double.parseDouble(aux);
-
-        if (salario <= 965.67 && salario <= 1609.45) {
-            
-
-        }
+        
+            if (salario >= 465) {
+                if (salario <= 965.67) {
+                    aliquota = 0.08;
+                    valordesconto = salario * 8 / 100;
+                    valorliquido = salario - valordesconto;
+                }
+                if (salario >= 965.68 && salario <= 1604.95) {
+                    aliquota = 0.09;
+                    valordesconto = salario * 9 / 100;
+                    valorliquido = salario - valordesconto;
+                }
+                if (salario >= 1604.96 && salario <= 3218.90) {
+                    aliquota = 0.11;
+                    valordesconto = salario * 11 / 100;
+                    valorliquido = salario - valordesconto;
+                }
+                JOptionPane.showMessageDialog(null, "\n Salário: " + salario + "\n Aliquota: " + aliquota * 100 + " % \n Valor Descontado: " + valordesconto + "\n Valor Líquido: " + valorliquido, "Calculo do INSS", -1);
+            } else {
+            JOptionPane.showMessageDialog(null, "O Salário informado é inferior a R$ 465,00.", "Salário Inválido", -1);
+            System.exit(0);
+            }
     }
+
 }
