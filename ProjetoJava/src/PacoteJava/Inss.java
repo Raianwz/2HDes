aa443b0a8a7192833ca9de0dfc3377e0ca2b3762
raianwz <raian03@live.com>
2020-03-16 18:29:52 -0300

    Pequenos Ajustes no Inss

--- ProjetoJava/src/PacoteJava/Inss.java
+++ ProjetoJava/src/PacoteJava/Inss.java
@@ -7,12 +7,12 @@ public class Inss {
     public static void main(String[] args) {
         String aux = " ";
         double salario = 0, aliquota = 0, valordesconto = 0, valorliquido = 0;
-        
+        try{
         aux = JOptionPane.showInputDialog(null, "Digite seu salário", "Salário", -1);
         aux = aux.replace(',', '.');
         salario = Double.parseDouble(aux);
         
-            if (salario >= 465) {
+            if (salario >= 465 && salario < 3218.90) {
                 if (salario <= 965.67) {
                     aliquota = 0.08;
                     valordesconto = salario * 8 / 100;
@@ -28,11 +28,13 @@ public class Inss {
                     valordesconto = salario * 11 / 100;
                     valorliquido = salario - valordesconto;
                 }
-                JOptionPane.showMessageDialog(null, "\n Salário: " + salario + "\n Aliquota: " + aliquota * 100 + " % \n Valor Descontado: " + valordesconto + "\n Valor Líquido: " + valorliquido, "Calculo do INSS", -1);
-            } else {
-            JOptionPane.showMessageDialog(null, "O Salário informado é inferior a R$ 465,00.", "Salário Inválido", -1);
-            System.exit(0);
+                JOptionPane.showMessageDialog(null, "\n Salário: R$ " + salario + "\n Aliquota: " + aliquota * 100 + " % \n Valor Descontado: R$ " + valordesconto + "\n Valor Líquido: R$ " + valorliquido, "Calculo do INSS", -1);
             }
+        }catch(NumberFormatException ex){
+            JOptionPane.showMessageDialog(null, "O Salário informado é invalido, por favor insira apenas números", "Salário Inválido", -1);
+            System.exit(0);
+        }
+            
     }
 
 }
